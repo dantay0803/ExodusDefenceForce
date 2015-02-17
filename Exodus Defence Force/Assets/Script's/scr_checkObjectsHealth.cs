32fb3f976ac7a2942693b029c340e888bb716dac
dantay0803 <dantay0803@hotmail.co.uk>
2015-02-17 21:45:58 +0000

    Project Init
    Init the project

---
+++ Exodus Defence Force/Assets/Script's/scr_checkObjectsHealth.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_checkObjectsHealth : MonoBehaviour {
+    //GetTheObjectsHealth
+    public float objectHealth;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Check if health is less than or equal to 0
+        if(objectHealth<=0){
+            //If true remove the instance of the object
+            Destroy(gameObject);
+        }
+	}
+}
